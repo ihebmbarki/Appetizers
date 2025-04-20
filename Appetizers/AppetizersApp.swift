515e8a0589cf3247cd04bd2888aa738f82ca460b
ihebmbarki <iheb.mbarki@esprit.tn>
2025-04-21 00:29:12 +0100

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Iheb Mbarki on 21/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
